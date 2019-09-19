fdbbb1ac5d12b5a1e69c706d51c6c696ca7ba176
amirdbt <hassanamir643@yahoo.com>
2019-09-19 12:18:37 +0100

    finishing sign in handlesbumit method

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import FormInput from "../FormInput/FormInput"
 import CustomButton from "../CustomButton/CustomButton"
-import { signInWithGoogle } from "../../firebase/firebase.utils"
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils"
 import "./SignIn.scss"
 
 class SignIn extends React.Component {
@@ -20,9 +20,18 @@ class SignIn extends React.Component {
     })
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault()
-
+    const { email, password } = this.state
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({
+        email: "",
+        password: ""
+      })
+    } catch (error) {
+      console.log(error)
+    }
     this.setState({
       email: "",
       password: ""
