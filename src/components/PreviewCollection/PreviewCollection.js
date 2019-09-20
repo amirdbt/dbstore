e4f4aa759ad43eba74960be13141356d9177497c
amirdbt <hassanamir643@yahoo.com>
2019-09-20 15:11:28 +0100

    updated shop data with new data and updated cart utils to group items in cart reducer

--- src/components/PreviewCollection/PreviewCollection.js
+++ src/components/PreviewCollection/PreviewCollection.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import './PreviewCollection.scss'
-import CollectionItem from '../CollectionItem/CollectionItem'
+import React from "react"
+import "./PreviewCollection.scss"
+import CollectionItem from "../CollectionItem/CollectionItem"
 
-const PreviewCollection =({title,items})=>{
-    return(
-        <div className='collection-preview'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <div className="preview">
-                {
-                    items
-                    .filter((item,idx) => idx < 4)
-                    .map(({id, ...otherItemProps}) =>{
-                     return   <CollectionItem key={id} {...otherItemProps} />
-                    })
-                }
-            </div>
-        </div>
-    )
+const PreviewCollection = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => {
+            return <CollectionItem key={item.id} item={item} />
+          })}
+      </div>
+    </div>
+  )
 }
 export default PreviewCollection
--- src/redux/cart/CartReducer.js
+++ src/redux/cart/CartReducer.js
@@ -1,16 +1,22 @@
 import CartActionTypes from "./CartTypes"
+import { addItemToCart } from "./CartUtils"
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.types) {
+  switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden
       }
-
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      }
     default:
       return state
   }
